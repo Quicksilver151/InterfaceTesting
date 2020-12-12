30c63528f2778febeed1fc281372008790849380
Quicksilver151 <Quicksilverhq151@gmail.com>
2020-12-12 21:38:22 +0500

    Actual Innit

---
+++ Code/Menu.gd
@@ -0,0 +1,19 @@
+extends MarginContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rect_size = get_viewport_rect().end
+	if Input.is_action_just_pressed("ui_accept") and get_focus_owner() == null:
+		$Layout/MidLayout/MarginContainer/Buttons/Play.grab_focus()
+	
