0a9576f7a0c4760ca0900de43e43618bc99cf496
Quicksilver151 <Quicksilverhq151@gmail.com>
2020-12-12 21:49:39 +0500

    menu button focus improved

--- Code/Menu.gd
+++ Code/Menu.gd
@@ -17,3 +17,11 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_accept") and get_focus_owner() == null:
 		$Layout/MidLayout/MarginContainer/Buttons/Play.grab_focus()
 	
+	for nodes in $Layout/MidLayout/MarginContainer/Buttons.get_children():
+		nodes.connect("pressed",self,"de_focus")
+		
+	
+
+func de_focus():
+	get_focus_owner().release_focus()
+
